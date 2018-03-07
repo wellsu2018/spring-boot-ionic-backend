7b90e63d4cbebd6e690f2ce8795cb10495eb1fa5
wellsu@gmail.com <wellsu@gmail.com>
2018-03-06 22:25:25 -0300

    Estado e Cidade

--- src/main/java/com/br/cursospring/CursospringApplication.java
+++ src/main/java/com/br/cursospring/CursospringApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.br.cursospring.domain.Categoria;
+import com.br.cursospring.domain.Cidade;
+import com.br.cursospring.domain.Estado;
 import com.br.cursospring.domain.Produto;
 import com.br.cursospring.repositories.CategoriaRepository;
+import com.br.cursospring.repositories.CidadeRepository;
+import com.br.cursospring.repositories.EstadoRepository;
 import com.br.cursospring.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,13 @@ public class CursospringApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
@@ -42,8 +53,20 @@ public class CursospringApplication implements CommandLineRunner{
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.save(Arrays.asList(cat1,cat2));
-
-		
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.save(Arrays.asList(est1,est2));
+		cidadeRepository.save(Arrays.asList(c1,c2,c3));
+		
 	}
 }
