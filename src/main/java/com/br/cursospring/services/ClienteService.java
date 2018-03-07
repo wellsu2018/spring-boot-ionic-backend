af5f25d4d1078d0d14c963212559b8e54e8adc0a
Wellington Alves <wellsu@gmail.com>
2018-03-07 16:51:09 -0300

    EndPoint /clientes/{id}

---
+++ src/main/java/com/br/cursospring/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.br.cursospring.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.cursospring.domain.Cliente;
+import com.br.cursospring.repositories.ClienteRepository;
+import com.br.cursospring.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+    @Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+ id +", Tipo: "+ Cliente.class.getName());
+		}
+		return obj;
+	
+	}
+
+}
