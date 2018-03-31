4025d4acf6862fdd2a2a801278d3e2f8668de7cd
wellsu@gmail.com <wellsu@gmail.com>
2018-03-31 17:37:11 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/br/cursospring/services/ClienteService.java
+++ src/main/java/com/br/cursospring/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.br.cursospring.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.br.cursospring.domain.Cliente;
+import com.br.cursospring.dto.ClienteDTO;
 import com.br.cursospring.repositories.ClienteRepository;
+import com.br.cursospring.services.exceptions.DataIntegrityException;
 import com.br.cursospring.services.exceptions.ObjectNotFoundException;
 
 
@@ -21,5 +29,48 @@ public class ClienteService {
 		return obj;
 	
 	}
+
+	public Cliente insert(Cliente obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}	
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		//Command utilizado na versao 2.x.x em diante
+		//repo.deleteById(id);
+		try {
+			repo.delete(id);	
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}	
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);  
+	}
+
+
 
 }
